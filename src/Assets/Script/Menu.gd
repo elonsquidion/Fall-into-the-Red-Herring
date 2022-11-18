2c49959303401ec9ae4fb688dadfae016c737287
Delon Khaerun Alief <16722052@mahasiswa.itb.ac.id>
2022-11-18 16:30:49 +0700

    audio

--- src/Assets/Script/Menu.gd
+++ src/Assets/Script/Menu.gd
@@ -4,14 +4,16 @@ const highscore_file = "user://highscore.txt"
 
 func _ready():
 	$Name.text = "Welcome, " + str(SilentWolf.Players.player_name) + "!"
+	print(SilentWolf.Players.player_name)
 	var player_scores = yield(SilentWolf.Scores.get_scores_by_player(SilentWolf.Players.player_name), "sw_player_scores_received")
 	print("Got player scores: " + str(player_scores))
 	print("Got: " + str(player_scores.size()) + " scores for player: " + str(SilentWolf.Players.player_name))
 	print("Does player have scores? " + str(player_scores.size() > 0))  
-	var label = get_node("Label")
 	if player_scores.size() > 0:
 		Global.highscore = player_scores[0]["score"]
-	label.text = "Highscore: " + str(Global.highscore)
+	else:
+		Global.highscore = 0
+	$"Highscore Label".text = "Highscore: " + str(Global.highscore)
 	get_tree().paused = false
 
 func _on_Start_pressed():
@@ -21,9 +23,6 @@ func _on_Start_pressed():
 func _on_Quit_pressed():
 	get_tree().quit()
 
-func _on_Button_pressed():
-	pass # Replace with function body.
-
 func _on_Leaderboard_pressed():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://addons/silent_wolf/Scores/Leaderboard.tscn")
@@ -32,3 +31,4 @@ func _on_Log_Out_pressed():
 	SilentWolf.Auth.logout_player()
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/auth_main.tscn")
+
